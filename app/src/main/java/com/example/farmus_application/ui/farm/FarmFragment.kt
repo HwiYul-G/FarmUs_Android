e3670eb8f2c3efd5256b7e7508dd5431b88f3cdf
Jihyeon An <84323575+jihyeonAnAn@users.noreply.github.com>
2023-02-04 16:02:39 +0900

    fix : commit 잘못한 부분 수정

--- app/src/main/java/com/example/farmus_application/ui/farm/FarmFragment.kt
+++ app/src/main/java/com/example/farmus_application/ui/farm/FarmFragment.kt
@@ -5,11 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import com.example.farmus_application.R
-import com.example.farmus_application.databinding.FragmentFarmBinding
-import com.example.farmus_application.databinding.TabLayoutTwoCategoriesBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,13 +18,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FarmFragment : Fragment() {
-
-    private lateinit var farmBinding : FragmentFarmBinding
-    private val tabTitleArray = arrayListOf<String>(
-        "분양받은 농장",
-        "보유중인 농장"
-    )
-
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -46,22 +35,7 @@ class FarmFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        farmBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_farm,container,false)
-
-        farmBinding.toolbar.toolbarMainTitleText.text = "내 농장"
-        farmBinding.toolbar.toolbarMainTitleText.setTextColor(resources.getColor(R.color.text_first))
-
-        val viewPager = farmBinding.viewPager
-        val tabLayout = farmBinding.tabLayout
-
-
-        viewPager.adapter = ViewPagerAdapter(childFragmentManager,lifecycle)
-
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = tabTitleArray[position]
-        }.attach()
-
-        return farmBinding.root
+        return inflater.inflate(R.layout.fragment_farm, container, false)
     }
 
     companion object {
