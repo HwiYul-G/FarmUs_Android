1a873844d8bd4be0a13669e53cf1ad216f7237f0
Jihyeon An <84323575+jihyeonAnAn@users.noreply.github.com>
2023-02-22 00:12:17 +0900

    Add: 마이페이지 생성

--- app/src/main/java/com/example/farmus_application/ui/account/ProfileSettingFragment.kt
+++ app/src/main/java/com/example/farmus_application/ui/account/ProfileSettingFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import com.example.farmus_application.R
-import com.example.farmus_application.databinding.FragmentMyPageBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,13 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
+ * Use the [ProfileSettingFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MyPageFragment : Fragment() {
-
-    private lateinit var myPageBinding: FragmentMyPageBinding
-
+class ProfileSettingFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -40,10 +35,7 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        myPageBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false)
-        val view = myPageBinding
-
-        return view.root
+        return inflater.inflate(R.layout.fragment_profile_setting, container, false)
     }
 
     companion object {
@@ -53,12 +45,12 @@ class MyPageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
+         * @return A new instance of fragment ProfileSettingFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
+            ProfileSettingFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
