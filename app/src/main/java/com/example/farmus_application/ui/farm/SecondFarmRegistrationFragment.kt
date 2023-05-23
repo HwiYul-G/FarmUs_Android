7b66a2c1592c7e3306e1271502bb5cf0e0be14d9
KJ-Hyeon <bark052526@gmail.com>
2023-05-23 13:58:28 +0900

    Add: FirstFarmRegistrationFragment 구현

---
+++ app/src/main/java/com/example/farmus_application/ui/farm/SecondFarmRegistrationFragment.kt
@@ -0,0 +1,18 @@
+package com.example.farmus_application.ui.farm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SecondFarmRegistrationFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
