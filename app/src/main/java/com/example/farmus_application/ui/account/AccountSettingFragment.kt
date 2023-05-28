d8b96e65a929123ca83bcb1280cb47acf02c58cb
Jihyeon An <jh011007@naver.com>
2023-05-29 02:55:32 +0900

    fix : MyPageFragment 복구

--- app/src/main/java/com/example/farmus_application/ui/account/AccountSettingFragment.kt
+++ app/src/main/java/com/example/farmus_application/ui/account/AccountSettingFragment.kt
@@ -1,30 +1,36 @@
 package com.example.farmus_application.ui.account
+
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.activity.OnBackPressedCallback
 import com.example.farmus_application.R
-import com.example.farmus_application.databinding.FragmentMyPageBinding
+import com.example.farmus_application.databinding.FragmentAccountSettingBinding
 import com.example.farmus_application.ui.MainActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 /**
  * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
+ * Use the [AccountSettingFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MyPageFragment : Fragment() {
+class AccountSettingFragment : Fragment() {
 
-    private lateinit var myPageBinding: FragmentMyPageBinding
+    private lateinit var binding : FragmentAccountSettingBinding
+    //뒤로가기 기능 구현
+    private lateinit var callback: OnBackPressedCallback
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -32,32 +38,45 @@ class MyPageFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        myPageBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false)
-        val view = myPageBinding
-
-        //bottomNavigationView 보이게
-        (activity as MainActivity).hideBottomNavigation(false)
-
-        //프로필 수정 버튼 누르면 ProfileSettingFragment로 이동
-        view.btnProfileSetting.setOnClickListener{
-            (activity as MainActivity).changeFragment(ProfileSettingFragment.newInstance("",""))
-        }
-        //설정 버튼 누르면 MyPageSettingFragment로 이동
-        view.btnSetting.setOnClickListener {
-            (activity as MainActivity).changeFragment(AccountSettingFragment.newInstance("",""))
-        }
-
-        view.listEnrollFarmer.setOnClickListener {
-            (activity as MainActivity).changeFragment(EnrollFarmerFragment.newInstance("",""))
-        }
-
-        return view.root
+        binding = FragmentAccountSettingBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.toolbarMainTitleText.apply {
+            text = "설정"
+        }
+        //툴바 백터튼 누르면 MyPageFragment로 이동
+        binding.toolbar.toolbarWithTitleBackButton.setOnClickListener {
+            (activity as MainActivity).changeFragment(MyPageFragment.newInstance("",""))
+        }
+    }
+
+    //뒤로가기 누르면 MyPageFragment로 이동
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                (activity as MainActivity).changeFragment(MyPageFragment.newInstance("",""))
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    //뒤로가기 누르면 MyPageFragment로 이동
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
+    }
+
 
     companion object {
         /**
@@ -66,12 +85,12 @@ class MyPageFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
+         * @return A new instance of fragment AccountSettingFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
+            AccountSettingFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
