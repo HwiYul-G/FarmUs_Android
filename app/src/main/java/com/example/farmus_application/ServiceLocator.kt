c39add6e7f766438eebd477f3dfc60a8634a6461
cgh <choigihongs@gmail.com>
2023-06-22 19:35:36 +0900

    Add: ServiceLocator 패턴을 이용한 apiClient 싱글톤 관리

--- app/src/main/java/com/example/farmus_application/ServiceLocator.kt
+++ app/src/main/java/com/example/farmus_application/ServiceLocator.kt
@@ -1,16 +1,17 @@
 package com.example.farmus_application
 
-import com.example.farmus_application.network.ApiClient
+import com.example.farmus_application.network.BaseApiClient
+import com.example.farmus_application.network.UserApiClient
+import retrofit2.Retrofit
 
 object ServiceLocator {
 
-    private var apiClient: ApiClient? = null
-
-    fun provideApiClient(): ApiClient {
-        return apiClient ?: run {
-            ApiClient.create().also { newApiClient ->
-                apiClient = newApiClient
-            }
-        }
+    private val baseApiClient: Retrofit by lazy {
+        BaseApiClient.create()
     }
+
+    val userApiClient: UserApiClient by lazy {
+        baseApiClient.create(UserApiClient::class.java)
+    }
+
 }
