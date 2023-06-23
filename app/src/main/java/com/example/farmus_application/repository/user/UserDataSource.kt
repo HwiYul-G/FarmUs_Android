d669a24462355c0717bab302e3fceac3dd2068e7
cgh <choigihongs@gmail.com>
2023-06-24 00:26:38 +0900

    Fix: User 도메인 리포지토리를 UserApiClient에 대응하여 코드 수정

--- app/src/main/java/com/example/farmus_application/repository/user/UserDataSource.kt
+++ app/src/main/java/com/example/farmus_application/repository/user/UserDataSource.kt
@@ -1,24 +1,34 @@
 package com.example.farmus_application.repository.user
 
-import com.example.farmus_application.model.user.*
+import com.example.farmus_application.model.user.findaccount.FindAccountRes
+import com.example.farmus_application.model.user.findpassword.FindPasswordRes
+import com.example.farmus_application.model.user.login.LoginReq
+import com.example.farmus_application.model.user.login.LoginRes
+import com.example.farmus_application.model.user.signup.SignUpReq
+import com.example.farmus_application.model.user.signup.SignUpRes
+import com.example.farmus_application.model.user.signup_verification.SignUpVerificationReq
+import com.example.farmus_application.model.user.signup_verification.SignUpVerificationRes
+import com.example.farmus_application.model.user.verification.VerificationReq
+import com.example.farmus_application.model.user.verification.VerificationRes
+import com.example.farmus_application.model.user.withdrawal.WithdrawalRes
+import retrofit2.Response
 
 interface UserDataSource {
 
     // suspend 함수 구현
-    suspend fun postUserSignup(params: SignUpReq): SignUpRes
+    suspend fun postUserSignup(params: SignUpReq): Response<SignUpRes>
 
-    suspend fun postUserLogin(params: LoginReq): LoginRes
+    suspend fun postUserLogin(params: LoginReq): Response<LoginRes>
 
-    suspend fun postUserSignupVerification(params : SignUpVerificationReq) : SignUpVerificationRes
+    suspend fun postUserSignupVerification(params : SignUpVerificationReq) : Response<SignUpVerificationRes>
 
-    suspend fun postUserVerification(params : VerificationReq) : VerificationRes
+    suspend fun postUserVerification(params : VerificationReq) : Response<VerificationRes>
 
-    suspend fun getUserFindAccount(params : FindAccountReq) : FindAccountRes
+    suspend fun getUserFindAccount(name : String, phoneNumber : String) : Response<FindAccountRes>
 
-    suspend fun getUserFindPassword(params : FindPasswordReq) : FindPasswordRes
+    suspend fun getUserFindPassword(userEmail : String) : Response<FindPasswordRes>
 
-    // userEmail을 검색해서 회원 탈퇴
-    // suspend fun patchUserWithdrawal(params : WithdrawalReq) : ResResult
+     suspend fun patchUserWithdrawal(userEmail: String) : Response<WithdrawalRes>
 
     // 농장 id를 입력받아서 해당 id를 post해야함
     // suspend fun postUserStarFarmid(params : FarmidReq) : FarmidRes
--- app/src/main/java/com/example/farmus_application/repository/user/UserRepository.kt
+++ app/src/main/java/com/example/farmus_application/repository/user/UserRepository.kt
@@ -1,42 +1,52 @@
 package com.example.farmus_application.repository.user
 
 import com.example.farmus_application.ServiceLocator
-import com.example.farmus_application.model.user.*
+import com.example.farmus_application.model.user.findaccount.FindAccountRes
+import com.example.farmus_application.model.user.findpassword.FindPasswordRes
+import com.example.farmus_application.model.user.login.LoginReq
+import com.example.farmus_application.model.user.login.LoginRes
+import com.example.farmus_application.model.user.signup.SignUpReq
+import com.example.farmus_application.model.user.signup.SignUpRes
+import com.example.farmus_application.model.user.signup_verification.SignUpVerificationReq
+import com.example.farmus_application.model.user.signup_verification.SignUpVerificationRes
+import com.example.farmus_application.model.user.verification.VerificationReq
+import com.example.farmus_application.model.user.verification.VerificationRes
+import com.example.farmus_application.model.user.withdrawal.WithdrawalRes
 import com.example.farmus_application.network.UserApiClient
+import retrofit2.Response
 
 class UserRepository(
     private val userApiClient: UserApiClient = ServiceLocator.userApiClient
 ): UserDataSource {
 
-    override suspend fun postUserSignup(params: SignUpReq): SignUpRes {
+    override suspend fun postUserSignup(params: SignUpReq): Response<SignUpRes> {
         return userApiClient.postSignUp(params = params)
     }
 
-    override suspend fun postUserLogin(params: LoginReq): LoginRes {
+    override suspend fun postUserLogin(params: LoginReq): Response<LoginRes> {
         return userApiClient.postLogin(params = params)
     }
 
-    override suspend fun postUserSignupVerification(params : SignUpVerificationReq): SignUpVerificationRes {
+    override suspend fun postUserSignupVerification(params : SignUpVerificationReq): Response<SignUpVerificationRes> {
         return userApiClient.postUserSignupVerification(params = params)
     }
 
-    override suspend fun postUserVerification(params : VerificationReq): VerificationRes {
-        return userApiClient.postUserVerification(verificationNumber = verificationNumber)
+    override suspend fun postUserVerification(params : VerificationReq): Response<VerificationRes> {
+        return userApiClient.postUserVerification(params = params)
     }
 
     // == get과 patch에 대한
-    override suspend fun getUserFindAccount(params : FindAccountReq): FindAccountRes {
-        return userApiClient.getUserFindAccount(params = params)
+    override suspend fun getUserFindAccount(name : String, phoneNumber : String): Response<FindAccountRes> {
+        return userApiClient.getUserFindAccount(name = name, phoneNumber = phoneNumber)
     }
 
-    override suspend fun getUserFindPassword(params : FindPasswordReq): FindPasswordRes {
-        return userApiClient.getUserFindPassword(params = params)
+    override suspend fun getUserFindPassword(userEmail : String): Response<FindPasswordRes> {
+        return userApiClient.getUserFindPassword(userEmail = userEmail)
     }
 
-    // data class 이름 임시로 해놓았습니다.
-//    override suspend fun patchUserWithdrawal(params : WithdrawalReq): ResResult{
-//        return userApiClient.patchUserWithdrawal(params = params)
-//    }
+    override suspend fun patchUserWithdrawal(userEmail: String): Response<WithdrawalRes>{
+        return userApiClient.patchUserWithdrawal(userEmail = userEmail)
+    }
 
 //    override suspend fun postUserStarFarmid(params : StarFarmidReq): StarFarmidRes {
 //        return userApiClient.postUserStarFarmid(params = params)
