b6226c2ed90482f271c4e4d1e6a96b6259ead855
Y <hwiyul_g@daum.net>
2023-06-28 20:36:02 +0900

    "Add : FindAccountViewModel에 Login관련 LiveData 추가 생성 및 휴대폰으로 인증번호 발송 관련 발송 된 정보와 이름, 전화번호 기반으로 id 알려주는 함수 작성"

--- app/src/main/java/com/example/farmus_application/viewmodel/findAccount/FindAccountViewModel.kt
+++ app/src/main/java/com/example/farmus_application/viewmodel/findAccount/FindAccountViewModel.kt
@@ -1,17 +1,78 @@
 package com.example.farmus_application.viewmodel.findAccount
 
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.farmus_application.model.user.findaccount.FindAccountRes
+import com.example.farmus_application.model.user.signup_verification.SignUpVerificationReq
+import com.example.farmus_application.model.user.signup_verification.SignUpVerificationRes
+import com.example.farmus_application.model.user.verification.VerificationReq
+import com.example.farmus_application.model.user.verification.VerificationRes
 import com.example.farmus_application.repository.user.UserRepository
 import kotlinx.coroutines.launch
 
 class FindAccountViewModel : ViewModel() {
     private val userRepository = UserRepository()
+
+    val authCodeResponse : MutableLiveData<SignUpVerificationRes> = MutableLiveData()
+    val verificationResponse : MutableLiveData<VerificationRes> = MutableLiveData()
     val findAccountResponse : MutableLiveData<FindAccountRes> = MutableLiveData()
+
+
+    fun postUserSignupVerification(params : SignUpVerificationReq){
+        viewModelScope.launch {
+            try{
+                val response = userRepository.postUserSignupVerification(params)
+                if(response.isSuccessful){
+                    Log.d("postUserSignupVerification Succeess : ", response.body().toString())
+                    val responseBody = response.body()
+                    responseBody?.let {
+                        if(it.result){
+                            // 인증번호 발송 성공
+                            authCodeResponse.postValue(it)
+                            Log.d("send code : ", response.body().toString())
+                        }else{
+                            // 인증번호 발송 실패
+                            authCodeResponse.postValue(it)
+                            Log.d("can't send code : ", response.body().toString())
+                        }
+                    }
+                }else{
+                    response.message()
+                }
+            } catch (e : Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun postUserVerification(params : VerificationReq){
+        viewModelScope.launch {
+            try{
+                val response = userRepository.postUserVerification(params)
+                if(response.isSuccessful){
+                    Log.d("postUserVerification Succeess : ", response.body().toString())
+                    val responseBody = response.body()
+                    responseBody?.let {
+                        if(it.result){
+                            // 아이디 찾기 성공
+                            verificationResponse.postValue(it)
+                            Log.d("email received : ", response.body().toString())
+                        }else{
+                            // 인증번호 발송 실패
+                            verificationResponse.postValue(it)
+                            Log.d("can't be received email : ", response.body().toString())
+                        }
+                    }
+                }else{
+                    response.message()
+                }
+            } catch (e : Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 
     fun findAccount(name : String, phone : String){
         viewModelScope.launch{
