0985264da9ebe21c19e3eeadd6eb56f1df47c23c
Y <hwiyul_g@daum.net>
2023-06-29 17:40:40 +0900

    Add : MyPageRes, 전부 result : bool만 가진 객체를 받아오므로 하나로 처리

---
+++ app/src/main/java/com/example/farmus_application/model/mypage/MyPageRes.kt
@@ -0,0 +1,7 @@
+package com.example.farmus_application.model.mypage
+
+import com.google.gson.annotations.SerializedName
+
+data class MyPageRes(
+    @SerializedName("result") val result: Boolean
+)
