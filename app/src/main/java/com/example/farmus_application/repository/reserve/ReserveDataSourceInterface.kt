4998dff6ea956b579c40f25415cd991f868abe84
cgh <choigihongs@gmail.com>
2023-07-03 16:36:38 +0900

    Add: Reserve 데이터 클래스 및 api 요청 작성

---
+++ app/src/main/java/com/example/farmus_application/repository/reserve/ReserveDataSourceInterface.kt
@@ -0,0 +1,25 @@
+package com.example.farmus_application.repository.reserve
+
+import com.example.farmus_application.model.reserve.cancel.ReserveCancelRes
+import com.example.farmus_application.model.reserve.client_list.ReserveClientListRes
+import com.example.farmus_application.model.reserve.farm_list.ReserveFarmListRes
+import com.example.farmus_application.model.reserve.request.ReserveRequestReq
+import com.example.farmus_application.model.reserve.request.ReserveRequestRes
+import com.example.farmus_application.model.reserve.status.ReserveStatusRes
+import retrofit2.Response
+
+interface ReserveDataSourceInterface {
+    enum class Status{
+        HOLD, ACCEPT, DENIED
+    }
+
+    suspend fun postReserveRequest(params: ReserveRequestReq): Response<ReserveRequestRes>
+
+    suspend fun getReservedFarmList(farmId: String): Response<ReserveFarmListRes>
+
+    suspend fun getReserveClientList(email: String): Response<ReserveClientListRes>
+
+    suspend fun putReserveCancel(reserveId: String): Response<ReserveCancelRes>
+
+    suspend fun putReserveStatus(status: Status, reserveId: String): Response<ReserveStatusRes>
+}
