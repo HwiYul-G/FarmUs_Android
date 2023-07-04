4b1d44476aa8eeaad95ee30c2bb27998c4877b55
Y <hwiyul_g@daum.net>
2023-07-04 22:39:09 +0900

    MODIFY : API 변경에 따른 FindAccout값을 받아오는 Response data class와 ViewModel 변경

--- app/src/main/java/com/example/farmus_application/model/user/findaccount/FindAccountRes.kt
+++ app/src/main/java/com/example/farmus_application/model/user/findaccount/FindAccountRes.kt
@@ -3,5 +3,10 @@ package com.example.farmus_application.model.user.findaccount
 import com.google.gson.annotations.SerializedName
 
 data class FindAccountRes(
-    @SerializedName("result") val result: Boolean
+    @SerializedName("result") val result: Boolean,
+    @SerializedName("code") val code : Int,
+    @SerializedName("message") val message : String,
+    @SerializedName("Name") val name : String,
+    @SerializedName("Email") val email : String,
+    @SerializedName("Status") val status : String
 )
