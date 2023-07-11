2df45a6edd7ef93f238c256fc6ba7a7a48edee16
KJ-Hyeon <bark052526@gmail.com>
2023-07-11 17:52:51 +0900

    Add: home에서 농장을 클릭하여 넘어올때, 받아오는 FarmId를 사용하여 ViewModel의 getFarmDetail를 호출하고 해당 농장의 상세 정보를 ViewModel의 farmDetail을 관찰하여 페이지에 정보를 세팅

--- app/src/main/java/com/example/farmus_application/ui/farm/FarmDetailFragment.kt
+++ app/src/main/java/com/example/farmus_application/ui/farm/FarmDetailFragment.kt
@@ -7,35 +7,46 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.farmus_application.R
 import com.example.farmus_application.databinding.FragmentFarmDetailBinding
 import com.example.farmus_application.ui.MainActivity
+import com.example.farmus_application.viewmodel.farm.FarmDetailViewModel
 
 class FarmDetailFragment: Fragment() {
 
-    private lateinit var farmDetailBinding: FragmentFarmDetailBinding
+    private lateinit var binding: FragmentFarmDetailBinding
+    private lateinit var farmDetailViewModel: FarmDetailViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        farmDetailBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_farm_detail, container, false)
-        val view = farmDetailBinding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_farm_detail, container, false)
+        farmDetailViewModel = ViewModelProvider(this)[FarmDetailViewModel::class.java]
+        val farmId = arguments?.getInt("farmId") ?: 0
 
-        farmDetailBinding.farmDetailRequestCalendar.setOnClickListener {
+        farmDetailViewModel.getFarmDetail(farmId)
+
+        binding.farmDetailRequestCalendar.setOnClickListener {
             val bottomSheetDialog = CalendarBottomSheetDialog()
             bottomSheetDialog.show(parentFragmentManager, "bottomSheetDialog")
         }
 
-        farmDetailBinding.farmDetailToolbar.toolbarWithoutTitleBackButton.apply {
+        binding.farmDetailToolbar.toolbarWithoutTitleBackButton.apply {
             setBackgroundColor(Color.TRANSPARENT)
             setImageResource(R.drawable.back_vector_image_white)
             setOnClickListener { (activity as MainActivity).changeFragment(FarmFragment()) }
         }
 
+        farmDetailViewModel.farmDetail.observe(viewLifecycleOwner) { detailRes ->
+            val result = detailRes.result
+            // 값 할당을 xml에서 해주는 방식으로 진행
+        }
 
-        return view.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
