5513ba55442623af2e430216d68386136f1c95fd
Y <hwiyul_g@daum.net>
2023-07-13 17:26:03 +0900

    Add : 날라오는 데이터 형식이 다르기 때문에 다른 어뎁터 필요 -> SearchedFarmRVAdapter 추가 + HomeSearchVM 추가

---
+++ app/src/main/java/com/example/farmus_application/viewmodel/home/HomeSearchViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.farmus_application.viewmodel.home
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.farmus_application.model.farm.search.SearchedFarm
+import com.example.farmus_application.repository.farm.FarmRepository
+import kotlinx.coroutines.launch
+
+class HomeSearchViewModel : ViewModel() {
+
+    private val farmRepo = FarmRepository()
+    val searchedResultBoolean : MutableLiveData<Boolean> = MutableLiveData()
+    val searchedFarmResponse : MutableLiveData<List<SearchedFarm>> = MutableLiveData()
+
+
+    fun getFarmSearchKeyword(keyword : String) {
+        viewModelScope.launch {
+            try {
+                val response = farmRepo.getFarmSearchKeyword(keyword)
+                if(response.isSuccessful){
+                    response.body()?.let {
+                        if(it.result){
+                            val serachedFarmList = mutableListOf<SearchedFarm>()
+                            for(farm in it.farms){
+                                serachedFarmList.add(
+                                    SearchedFarm(
+                                        farm.FarmID,
+                                        farm.Name,
+                                        farm.Price,
+                                        farm.SquaredMeters,
+                                        farm.LocationBig,
+                                        farm.LocationMid,
+                                        farm.LocationSmall,
+                                        farm.Likes,
+                                        farm.Picture_url)
+                                )
+                            }
+                            searchedFarmResponse.postValue(serachedFarmList)
+                            searchedResultBoolean.postValue(true)
+                        }else{
+                            Log.d("FarmSearch Success : ", response.body().toString())
+                            searchedResultBoolean.postValue(false)
+                        }
+                    }
+                }else{
+                    Log.d("FarmSearch Failed : ", response.body().toString())
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+}
