48238579dc0ffc755e5487b0ca8890d4736f9ed6
Y <hwiyul_g@daum.net>
2023-07-20 23:37:59 +0900

    Add : API 명세 오류로 기존에 없었던  Response data class 추가.

--- app/src/main/java/com/example/farmus_application/model/mypage/MyPageRes.kt
+++ app/src/main/java/com/example/farmus_application/model/mypage/MyPageRes.kt
@@ -2,6 +2,19 @@ package com.example.farmus_application.model.mypage
 
 import com.google.gson.annotations.SerializedName
 
+// backend에서 결과 true, false 날라오는 값의 이름이 다름
 data class MyPageRes(
-    @SerializedName("result") val result: Boolean
+    @SerializedName("result") val result: Boolean, // 닉네임 수정, 이름 수정
+    @SerializedName("code") val code: Int,
+    @SerializedName("message") val message: String
+)
+
+data class MyPagePasswordRes(
+    @SerializedName("isSuccess") val isSuccess: Boolean,
+    @SerializedName("code") val code: Int,
+    @SerializedName("message") val message: String
+)
+
+data class MyPagePhoneNumberRes(
+    @SerializedName("result") val result : Boolean,
 )
