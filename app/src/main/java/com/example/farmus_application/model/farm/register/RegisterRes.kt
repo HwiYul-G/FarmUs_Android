b82b5e5a26a304d19c2f822f5583f3864862d48b
KJ-Hyeon <bark052526@gmail.com>
2023-07-31 15:52:21 +0900

    Add: RegisterRes 데이터 모델에 AccessToken 데이터 추가

--- app/src/main/java/com/example/farmus_application/model/farm/register/RegisterRes.kt
+++ app/src/main/java/com/example/farmus_application/model/farm/register/RegisterRes.kt
@@ -3,5 +3,6 @@ package com.example.farmus_application.model.farm.register
 import com.google.gson.annotations.SerializedName
 
 data class RegisterRes(
-    @SerializedName("result") val result: Boolean
+    @SerializedName("result") val result: Boolean,
+    @SerializedName("accesstoken") val accessToken: String
 )
