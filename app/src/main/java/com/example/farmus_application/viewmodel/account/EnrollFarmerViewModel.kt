dcbc291ba2fcac21f93fb89dbb44e7978d48f1c1
KJ-Hyeon <bark052526@gmail.com>
2023-08-01 17:00:20 +0900

    Add: 사용자가 농장주 등록을 하면 서버로부터 새로운 토큰값을 받아와서 UserPrefsStorage의 accessToken을 업데이트 시켜줌으로써 사용자의 role을 업데이트 한다

---
+++ app/src/main/java/com/example/farmus_application/viewmodel/account/EnrollFarmerViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.farmus_application.viewmodel.account
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.farmus_application.model.farm.register.RegisterRes
+import com.example.farmus_application.repository.UserPrefsStorage
+import com.example.farmus_application.repository.farm.FarmRepository
+import com.kakao.sdk.user.model.User
+import kotlinx.coroutines.launch
+
+class EnrollFarmerViewModel: ViewModel() {
+
+    private val farmRepository = FarmRepository()
+
+    private var _farmRegister = MutableLiveData<Boolean>()
+    var farmRegister: LiveData<Boolean> = _farmRegister
+
+    fun patchFarmRegister(token: String) {
+        viewModelScope.launch {
+            try {
+                val response = farmRepository.patchFarmRegister(token)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        UserPrefsStorage.accessToken = it.accessToken
+                        _farmRegister.postValue(it.result)
+                    }
+                } else {
+                    Log.e("농장주 등록 실패:","${response.body()}")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+}
