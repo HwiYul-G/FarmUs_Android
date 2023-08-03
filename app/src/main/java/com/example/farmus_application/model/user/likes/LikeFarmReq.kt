ff4a5b21fd1730beafe345f8501c088797276f9b
Y <hwiyul_g@daum.net>
2023-08-03 19:20:40 +0900

    ADD : 농장찜하기, 찜취소하기를 위한 데이터 클레스

---
+++ app/src/main/java/com/example/farmus_application/model/user/likes/LikeFarmReq.kt
@@ -0,0 +1,8 @@
+package com.example.farmus_application.model.user.likes
+
+import com.google.gson.annotations.SerializedName
+
+data class LikeFarmReq(
+    @SerializedName("email") val email: String,
+    @SerializedName("farmid") val farmid: Int,
+)
