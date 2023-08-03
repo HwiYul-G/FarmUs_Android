e1832c01c7becd50350643ccbc31d5cc886f835d
Y <hwiyul_g@daum.net>
2023-08-03 23:43:12 +0900

    FIX : data type 중 LocationSmall 없는 경우 미작동으로 nullable 처리.

--- app/src/main/java/com/example/farmus_application/model/favorite/FavoriteFarm.kt
+++ app/src/main/java/com/example/farmus_application/model/favorite/FavoriteFarm.kt
@@ -9,7 +9,7 @@ data class FavoriteFarm(
     @SerializedName("SquaredMeters") val SquaredMeters: Int,
     @SerializedName("LocationBig") val LocationBig: String,
     @SerializedName("LocationMid") val LocationMid: String,
-    @SerializedName("LocationSmall") val LocationSmall: String,
+    @SerializedName("LocationSmall") val LocationSmall: String?,
     @SerializedName("Likes") val Likes: Int,
     @SerializedName("Picture_url") val PictureUrl: String?,
 )
