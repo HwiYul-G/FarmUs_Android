61c0690d40caecb28bd39a07fa8fd19b7b06a8ab
kim <hwiyul_g@daum.net>
2023-08-27 12:23:54 +0900

    MODIFY : 서버를 http에서 https로 바꾸면서 javax.net.ssl.SSLPeerUnverifiedException: Hostname not verified 라는 오류가 발생 이에 대한 처리로 ssl 인증서 없이 https로 우회해 통신하는 처리 작성.

--- app/src/main/java/com/farm/farmus_application/network/BaseApiClient.kt
+++ app/src/main/java/com/farm/farmus_application/network/BaseApiClient.kt
@@ -1,5 +1,6 @@
 package com.farm.farmus_application.network
 
+import android.util.Log
 import com.farm.farmus_application.repository.UserPrefsStorage
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -7,11 +8,16 @@ import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 interface BaseApiClient {
 
     companion object {
-        private const val baseUrl = "http://118.67.135.247:3000/"
+        private const val baseUrl = "https://118.67.135.247/"
         private val jwtToken = UserPrefsStorage.accessToken ?: ""
 
         fun create(): Retrofit {
@@ -19,7 +25,7 @@ interface BaseApiClient {
                 level = HttpLoggingInterceptor.Level.BASIC
             }
 
-            val client = OkHttpClient.Builder()
+            val client = unsafeOkHttpClient()
                 .addInterceptor(logger)
                 .addInterceptor(AppInterceptor())
                 .build()
@@ -31,8 +37,43 @@ interface BaseApiClient {
                 .build()
         }
 
+        private fun unsafeOkHttpClient(): OkHttpClient.Builder {
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(
+                    chain: Array<out java.security.cert.X509Certificate>?,
+                    authType: String?
+                ) {
+
+                }
+
+                override fun checkServerTrusted(
+                    chain: Array<out java.security.cert.X509Certificate>?,
+                    authType: String?
+                ) {
+
+                }
+
+                override fun getAcceptedIssuers(): Array<out java.security.cert.X509Certificate>? {
+                    return arrayOf()
+                }
+            })
+
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            val sslSocketFactory = sslContext.socketFactory
+
+
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            builder.hostnameVerifier { hostname, session -> true }
+
+            return builder
+        }
+
         class AppInterceptor : Interceptor {
             override fun intercept(chain: Interceptor.Chain): Response {
+                Log.d("BASEAPICLIENT", "token : ${jwtToken.toString()}")
                 val newRequest = chain.request().newBuilder()
                     .addHeader("token", jwtToken.toString())
                 return chain.proceed(newRequest.build())
