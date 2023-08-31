426c6f2fbc5ab1e5e95cc4ce521ad552f89280dc
kim <hwiyul_g@daum.net>
2023-08-31 15:38:44 +0900

    Add : Farmer가 요청받을 때의 화면 + 어뎁터 + 화면 넘기기, 다이얼 로그 등 기능 추가 (키보드 hide를 안시키고 back 버튼 전환시 이전 화면에 영향을 주는 사항이 있었음)

--- app/src/main/java/com/farm/farmus_application/ui/message/ChatTab2Fragment.kt
+++ app/src/main/java/com/farm/farmus_application/ui/message/ChatTab2Fragment.kt
@@ -9,27 +9,21 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.farm.farmus_application.R
 import com.farm.farmus_application.databinding.FragmentChatTab2Binding
+import com.farm.farmus_application.ui.MainActivity
 import com.farm.farmus_application.ui.message.adapter.ChatTab2ApprovedRVAdapter
 import com.farm.farmus_application.ui.message.adapter.ChatTab2RVAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatTab2Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatTab2Fragment : Fragment() {
 
     private lateinit var binding: FragmentChatTab2Binding
 
-    private lateinit var tab2Adapter : ChatTab2RVAdapter
-    private lateinit var tab2ApprovedAdapter : ChatTab2ApprovedRVAdapter
+    private lateinit var tab2Adapter: ChatTab2RVAdapter
+    private lateinit var tab2ApprovedAdapter: ChatTab2ApprovedRVAdapter
 
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -44,11 +38,9 @@ class ChatTab2Fragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat_tab2, container, false)
         val view = binding
-
         return view.root
     }
 
@@ -57,38 +49,98 @@ class ChatTab2Fragment : Fragment() {
 
         val waitItem = mutableListOf<RVTab2DataModel>()
         tab2Adapter = ChatTab2RVAdapter()
+        // adatper 내부에 있는 요소들 클릭 이벤트( 어뎁터 안의 interface 명세도 수정 필요)
+        tab2Adapter.setOnItemClick(object : ChatTab2RVAdapter.OnItemClickListener {
+            override fun itemClick() {
+                // 필요시 bundle 등 추가
+                val farmerChatFragment = FarmerChatFragment()
+                (activity as MainActivity).changeFragmentAddToBackStack(farmerChatFragment)
+            }
+        })
+
         tab2Adapter.submitList(waitItem)
         binding.rvChatWait.adapter = tab2Adapter
         binding.rvChatWait.layoutManager = LinearLayoutManager(requireContext())
-        waitItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        waitItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        waitItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        waitItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        waitItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
+        waitItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        waitItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        waitItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        waitItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        waitItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
 
         val approveItem = mutableListOf<RVTab2DataModel>()
         tab2ApprovedAdapter = ChatTab2ApprovedRVAdapter()
         tab2ApprovedAdapter.submitList(approveItem)
         binding.rvChatApprove.adapter = tab2ApprovedAdapter
         binding.rvChatApprove.layoutManager = LinearLayoutManager(requireContext())
-        approveItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        approveItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        approveItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        approveItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
-        approveItem.add(RVTab2DataModel(R.drawable.farm_image_example, "서울 유일 농장", "2023.02.03~2023.05.07"))
+        approveItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        approveItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        approveItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        approveItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
+        approveItem.add(
+            RVTab2DataModel(
+                R.drawable.farm_image_example,
+                "서울 유일 농장",
+                "2023.02.03~2023.05.07"
+            )
+        )
 
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatTab2Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ChatTab2Fragment().apply {
--- app/src/main/java/com/farm/farmus_application/ui/message/adapter/ChatTab2RVAdapter.kt
+++ app/src/main/java/com/farm/farmus_application/ui/message/adapter/ChatTab2RVAdapter.kt
@@ -3,13 +3,25 @@ package com.farm.farmus_application.ui.message.adapter
 import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.farm.farmus_application.databinding.RvChatTab2ItemBinding
+import com.farm.farmus_application.ui.message.ChatBottomSheetFragment
 import com.farm.farmus_application.ui.message.RVTab2DataModel
 
-class ChatTab2RVAdapter() : ListAdapter<RVTab2DataModel, ChatTab2RVAdapter.ViewHolder>(diffUtil) {
+class ChatTab2RVAdapter : ListAdapter<RVTab2DataModel, ChatTab2RVAdapter.ViewHolder>(diffUtil) {
+
+    private var listener: OnItemClickListener? = null
+
+    interface OnItemClickListener {
+        fun itemClick()
+    }
+
+    fun setOnItemClick(listener: OnItemClickListener) {
+        this.listener = listener
+    }
 
     private lateinit var chatTab2Binding: RvChatTab2ItemBinding
 
@@ -36,11 +48,26 @@ class ChatTab2RVAdapter() : ListAdapter<RVTab2DataModel, ChatTab2RVAdapter.ViewH
             chatTab2Binding.rvChatImg.setImageResource(item.farm_image)
             chatTab2Binding.rvChatName.text = item.farm_name
             chatTab2Binding.rvChatPeriodDay.text = item.farm_period
+
+            chatTab2Binding.rvChatHistory.setOnClickListener {
+                val chatBottomSheetFragment = ChatBottomSheetFragment()
+
+                chatBottomSheetFragment.show(
+                    (chatTab2Binding.root.context as AppCompatActivity).supportFragmentManager,
+                    chatBottomSheetFragment.tag
+                )
+            }
+
+            // TODO : 필요시 root가 아니라 next_btn clcik 이벤트 등으로 변경해서 처리
+            chatTab2Binding.root.setOnClickListener {
+                listener?.itemClick()
+            }
+
         }
     }
 
-    companion object{
-        val diffUtil = object: DiffUtil.ItemCallback<RVTab2DataModel>(){
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<RVTab2DataModel>() {
             override fun areItemsTheSame(
                 oldItem: RVTab2DataModel,
                 newItem: RVTab2DataModel
