fa563bc2c969dd5ad6bde1bd34d36b4c4d36f8a4
KJ-Hyeon <bark052526@gmail.com>
2023-09-05 21:17:00 +0900

    Add: 농장주가 예약 불가능한 날짜를 설정 할 수 있는 fragment_management_calendar 추가

---
+++ app/src/main/java/com/farm/farmus_application/ui/farm/ManagementCalendarFragment.kt
@@ -0,0 +1,31 @@
+package com.farm.farmus_application.ui.farm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.farm.farmus_application.R
+import com.farm.farmus_application.databinding.FragmentManagementCalendarBinding
+
+class ManagementCalendarFragment: Fragment() {
+
+    private lateinit var binding: FragmentManagementCalendarBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_management_calendar, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
