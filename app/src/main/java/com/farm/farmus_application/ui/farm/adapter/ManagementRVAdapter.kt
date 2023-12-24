9a4935496dff5846b4354d160076e6069764cc46
kim <hwiyul_g@daum.net>
2023-12-24 16:56:28 +0900

    feat : 분양 불가 기간 등록된 것을 리사이클러뷰에 담기 위한 어뎁터 추가 + 각 기간의 삭제 버튼 클릭 이벤트를 받음

--- app/src/main/java/com/farm/farmus_application/ui/farm/adapter/ManagementRVAdapter.kt
+++ app/src/main/java/com/farm/farmus_application/ui/farm/adapter/ManagementRVAdapter.kt
@@ -8,14 +8,28 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.farm.farmus_application.R
 import com.farm.farmus_application.databinding.ItemManagementCalnedarBinding
-import com.farm.farmus_application.model.reserve.unbookable.UnBookableResult
+import com.farm.farmus_application.model.farm.unavailableDate.UnavailableDateInfo
 
-class ManagementRVAdapter: ListAdapter<UnBookableResult, ManagementRVAdapter.ManagementViewHolder>(diffUtil) {
+interface OnDeleteClickListener {
+    fun onDeleteClick(farmDateID: Int)
+}
+
+class ManagementRVAdapter(
+    private val onDeleteClickListener: OnDeleteClickListener
+) :
+    ListAdapter<UnavailableDateInfo, ManagementRVAdapter.ManagementViewHolder>(diffUtil) {
 
     private lateinit var binding: ItemManagementCalnedarBinding
 
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ManagementViewHolder {
-        binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_management_calnedar, parent, false)
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_management_calnedar,
+            parent,
+            false
+        )
         return ManagementViewHolder(binding)
     }
 
@@ -23,24 +37,34 @@ class ManagementRVAdapter: ListAdapter<UnBookableResult, ManagementRVAdapter.Man
         holder.bind(currentList[position])
     }
 
-    inner class ManagementViewHolder(private val binding: ItemManagementCalnedarBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: UnBookableResult) {
+    inner class ManagementViewHolder(private val binding: ItemManagementCalnedarBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: UnavailableDateInfo) {
+            val formattedStartDate = item.getFormattedStartDate()
+            val formattedEndDate = item.getFormattedEndDate()
+
             binding.unAvailableDate = item
+            binding.unavailableDateTextView.text = "$formattedStartDate~$formattedEndDate"
+
+
+            binding.unavailableClearButton.setOnClickListener {
+                onDeleteClickListener.onDeleteClick(item.FarmDateID)
+            }
         }
     }
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<UnBookableResult>() {
+        val diffUtil = object : DiffUtil.ItemCallback<UnavailableDateInfo>() {
             override fun areItemsTheSame(
-                oldItem: UnBookableResult,
-                newItem: UnBookableResult
+                oldItem: UnavailableDateInfo,
+                newItem: UnavailableDateInfo
             ): Boolean {
                 return oldItem === newItem
             }
 
             override fun areContentsTheSame(
-                oldItem: UnBookableResult,
-                newItem: UnBookableResult
+                oldItem: UnavailableDateInfo,
+                newItem: UnavailableDateInfo
             ): Boolean {
                 return oldItem == newItem
             }
