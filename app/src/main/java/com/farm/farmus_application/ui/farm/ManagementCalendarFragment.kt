075924f70d53441abe603e3a82ebbe49c5c4f634
kim <hwiyul_g@daum.net>
2023-12-24 16:57:32 +0900

    feat : 농장주가 이용 불가 기간 등록 및 삭제를 위해 관리하는 프래그먼트와 뷰모델 동작 추가

--- app/src/main/java/com/farm/farmus_application/ui/farm/ManagementCalendarFragment.kt
+++ app/src/main/java/com/farm/farmus_application/ui/farm/ManagementCalendarFragment.kt
@@ -1,5 +1,6 @@
 package com.farm.farmus_application.ui.farm
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,15 +10,22 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.farm.farmus_application.R
 import com.farm.farmus_application.databinding.FragmentManagementCalendarBinding
 import com.farm.farmus_application.model.reserve.unbookable.UnBookableResult
+import com.farm.farmus_application.ui.farm.adapter.ManagementRVAdapter
+import com.farm.farmus_application.ui.farm.adapter.OnDeleteClickListener
 import com.farm.farmus_application.viewmodel.calendar.CalendarViewModel
+import com.farm.farmus_application.viewmodel.farm.ManagementCalendarViewModel
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @AndroidEntryPoint
-class ManagementCalendarFragment: Fragment() {
+class ManagementCalendarFragment : Fragment(), OnDeleteClickListener {
 
     private lateinit var binding: FragmentManagementCalendarBinding
     private lateinit var firstSelectedDay: CalendarDay
@@ -25,13 +33,21 @@ class ManagementCalendarFragment: Fragment() {
     private var farmId = 0
     private var unBookDayList: List<UnBookableResult> = listOf()
     private val calendarViewModel: CalendarViewModel by viewModels()
+    private val managementCalendarViewModel: ManagementCalendarViewModel by viewModels()
+    private val managementRVAdapter = ManagementRVAdapter(this)
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_management_calendar, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_management_calendar,
+            container,
+            false
+        )
         farmId = arguments?.getInt("farmId") ?: 0
         return binding.root
     }
@@ -41,6 +57,7 @@ class ManagementCalendarFragment: Fragment() {
 
         initView()
 
+        // 뒤로 가기 버튼
         binding.managementCalendarBack.setOnClickListener {
             activity?.supportFragmentManager?.apply {
                 beginTransaction().remove(this@ManagementCalendarFragment).commit()
@@ -53,41 +70,99 @@ class ManagementCalendarFragment: Fragment() {
                 result.result?.let {
                     binding.managementCalendar.addDecorator(UnBookableDayDecorator(it))
                     unBookDayList = it
-                    Log.e("unBookDayList","$unBookDayList")
+                    Log.e("unBookDayList", "$unBookDayList")
                 }
             } else {
                 Toast.makeText(requireContext(), result.message, Toast.LENGTH_SHORT).show()
             }
         }
 
-        binding.managementCalendar.setOnDateChangedListener { widget, date, selected ->
+        binding.managementCalendar.setOnDateChangedListener { widget, date, _ ->
             widget.apply {
                 removeDecorators()
                 addDecorator(SelectedDecorator(date))
-                addDecorators(TodayDecorator(requireContext()), BeforeDayDecorator(),SelectedDecorator(date))
+                addDecorators(
+                    TodayDecorator(requireContext()),
+                    BeforeDayDecorator(),
+                    SelectedDecorator(date)
+                )
             }
             settingCalendarStartText(date)
-            switchDateFocus(true)
+            activateAddButton(false)
         }
 
         binding.managementCalendar.setOnRangeSelectedListener { widget, dates ->
             val dayList = mutableListOf<CalendarDay>().apply {
                 addAll(dates)
                 removeAt(0)
-                removeAt(this.size -1)
+                removeAt(this.size - 1)
             }
             if (firstSelectedDay != dates[0]) {
                 widget.addDecorator(SelectedDecorator(dates[0]))
                 settingCalendarStartText(dates[0])
             } else {
-                widget.addDecorator(SelectedDecorator(dates[dates.size -1]))
+                widget.addDecorator(SelectedDecorator(dates[dates.size - 1]))
             }
             if (dayList.size != 0) {
                 widget.addDecorator(RangeDayDecorator(dayList, requireContext()))
             }
-            settingCalendarLastText(dates[dates.size-1])
-            switchDateFocus(false)
+            settingCalendarLastText(dates[dates.size - 1])
+            activateAddButton(true)
+
         }
+
+        // '추가' 버튼 클릭 이벤트
+        binding.managementCalendarAdd.setOnClickListener {
+            if (binding.managementCalendarAdd.isSelected) {
+                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+                val calendar = Calendar.getInstance()
+                calendar.set(
+                    firstSelectedDay.year,
+                    firstSelectedDay.month - 1,
+                    firstSelectedDay.day
+                ) // CalendarDay의 month는 1을 빼야 합니다 (0-11 범위)
+                val unavailableStartDate = dateFormat.format(calendar.time)
+
+                calendar.set(lastSelectedDay.year, lastSelectedDay.month - 1, lastSelectedDay.day)
+                val unavailableEndDate = dateFormat.format(calendar.time)
+
+                managementCalendarViewModel.addUnavailableDate(
+                    farmId,
+                    unavailableStartDate,
+                    unavailableEndDate
+                )
+            }
+        }
+
+        // 이용 불가 기간 추가 버튼 클릭 후 결과 관찰
+        managementCalendarViewModel.isSuccessAddDate.observe(viewLifecycleOwner) {
+            if (it) {
+                managementCalendarViewModel.addUnavailableDate(
+                    farmId,
+                    firstSelectedDay.toString(),
+                    lastSelectedDay.toString()
+                )
+            }
+        }
+
+        managementCalendarViewModel.isSuccessDeleteDate.observe(viewLifecycleOwner) {
+            if (it) {
+                Toast.makeText(context, "삭제되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        // 이용 불가 기간 목록 전체 확인 -> 리사이클러 뷰로 아이템 넣기
+        managementCalendarViewModel.unavailableDateInfoList.observe(viewLifecycleOwner) {
+            managementRVAdapter.submitList(it)
+        }
+
+        managementCalendarViewModel.errorMessage.observe(viewLifecycleOwner) { errorMessage ->
+            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
     private fun initView() {
@@ -102,35 +177,50 @@ class ManagementCalendarFragment: Fragment() {
             setLeftArrow(R.drawable.calendar_back_button_vector)
             setRightArrow(R.drawable.calendar_forward_button_vector)
         }
+
+        binding.dateRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.dateRecyclerView.adapter = managementRVAdapter;
+
+        managementCalendarViewModel.getUnavailableDateList(farmId)
     }
 
     private fun settingCalendarStartText(date: CalendarDay) {
         binding.apply {
             calendarStartDayYear.text = date.year.toString()
-            calendarStartDayMonth.text = String.format("%02d",date.month)
+            calendarStartDayMonth.text = String.format("%02d", date.month)
             calendarStartDayDate.text = date.day.toString()
         }
         firstSelectedDay = date
     }
+
     private fun settingCalendarLastText(date: CalendarDay) {
         binding.apply {
             calendarLastDayYear.text = date.year.toString()
-            calendarLastDayMonth.text = String.format("%02d",date.month)
+            calendarLastDayMonth.text = String.format("%02d", date.month)
             calendarLastDayDate.text = date.day.toString()
         }
         lastSelectedDay = date
     }
-    private fun switchDateFocus(switch: Boolean) {
+
+    private fun activateAddButton(switch: Boolean) {
         if (switch) {
             binding.apply {
                 startDayConstraint.isSelected = true
                 lastDayConstraint.isSelected = false
+                managementCalendarAdd.setTextColor(Color.GREEN)
+                managementCalendarAdd.isSelected = true
             }
         } else {
             binding.apply {
                 startDayConstraint.isSelected = false
                 lastDayConstraint.isSelected = true
+                managementCalendarAdd.setTextColor(Color.GRAY)
+                managementCalendarAdd.isSelected = false
             }
         }
+    }
+
+    override fun onDeleteClick(farmDateID: Int) {
+        managementCalendarViewModel.deleteUnavailableDate(farmDateID, farmId)
     }
 }
---
+++ app/src/main/java/com/farm/farmus_application/viewmodel/farm/ManagementCalendarViewModel.kt
@@ -0,0 +1,101 @@
+package com.farm.farmus_application.viewmodel.farm
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.farm.farmus_application.model.farm.unavailableDate.UnavailableDateAdditionReq
+import com.farm.farmus_application.model.farm.unavailableDate.UnavailableDateInfo
+import com.farm.farmus_application.repository.farm.FarmDataSourceInterface
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+// 아키텍처 구조가 잘못 잡혀 있어서.. 이상하긴 하지만 원래는 Repo interface를 넘겨서 해야함.
+// DataSrcInterface라고 명명되어있지만 사실 Repo interface임.
+@HiltViewModel
+class ManagementCalendarViewModel @Inject constructor(
+    private val farmRepository: FarmDataSourceInterface,
+) : ViewModel() {
+    private val TAG = "ManagementCalendarViewModel"
+
+    private val _isSuccessAddDate = MutableLiveData<Boolean>()
+    val isSuccessAddDate: LiveData<Boolean> get() = _isSuccessAddDate
+
+    private val _unavailableDateInfoList = MutableLiveData<List<UnavailableDateInfo>>()
+    val unavailableDateInfoList: LiveData<List<UnavailableDateInfo>> = _unavailableDateInfoList
+
+    private val _isSuccessDeleteDate = MutableLiveData<Boolean>()
+    val isSuccessDeleteDate: LiveData<Boolean> = _isSuccessDeleteDate
+
+    private val _errorMessage = MutableLiveData<String>() // 에러 메시지 전달용 LiveData
+    val errorMessage: LiveData<String> = _errorMessage
+
+    fun addUnavailableDate(farmID: Int, unavailableStartDate: String, unavailableEndDate: String) {
+        viewModelScope.launch {
+            try {
+                // Retrofit 이용해서 API 호출 (로컬 정보를 서버로 보냄)
+                val response = farmRepository.postUnavailableDate(
+                    UnavailableDateAdditionReq(farmID, unavailableStartDate, unavailableEndDate)
+                )
+                if (response.isSuccessful) {
+                    // message와 farmDateID에 대한 처리 필요
+                    response.body()?.let {
+                        _isSuccessAddDate.postValue(it.isSuccess)
+
+                        // 추가가 성공적으로 이루어지면 목록을 업데이트(UI 바로 반영)
+                        if (it.isSuccess) {
+                            getUnavailableDateList(farmID)
+                        }
+
+                    }
+                } else {
+                    _errorMessage.value = "이용 불가 기간 추가에 오류가 있습니다."
+                    Log.e(TAG, response.errorBody()?.string() ?: response.message())
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun deleteUnavailableDate(farmDateID: Int, farmID: Int) {
+        viewModelScope.launch {
+            try {
+                val response = farmRepository.putDeleteUnavailableDate(farmDateID)
+                response.body()?.let {
+                    if (it.isSuccess) {
+                        _isSuccessDeleteDate.postValue(true)
+                        getUnavailableDateList(farmID)
+                    } else {
+                        _errorMessage.value = "삭제에 문제가 발생했습니다."
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+    fun getUnavailableDateList(farmID: Int) {
+        viewModelScope.launch {
+            try {
+                val response = farmRepository.getUnavailableDate(farmID)
+                response.body()?.let {
+                    if (it.isSuccess) {
+                        _unavailableDateInfoList.postValue(it.result)
+                    } else {
+                        _errorMessage.value = "이용 불가 기간 목록을 불러오는 데 문제가 발생했습니다."
+                        Log.e(TAG, response.errorBody()?.string() ?: response.message())
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+}
